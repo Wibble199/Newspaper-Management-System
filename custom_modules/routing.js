52865f85ee268f478c134babad2b0587b22fc2eb
Will Bennion <will.bennion@outlook.com>
2017-03-08 00:25:43 +0000

    Added a `requireAuth` function for routing calls.

--- custom_modules/routing.js
+++ custom_modules/routing.js
@@ -7,4 +7,22 @@ module.exports = function(app) {
 		else
 			res.sendFile(appDir + "/html/login.html");
 	});
+
+	/*app.get('/', requireAuth((req, res) => {
+		// Require auth use example
+	}));*/
 };
+
+/**
+ * Function that checks a request to ensure that it is authenticated with passport and if so calls the `next` function with (req, res) objects from the route. If not authenticated then uses the response object and sends back a 401 error.
+ * Designed to fit subtley and elegantly into an app.get or app.post etc. call.
+ * @param {function} next The function to be called if request is authenticated
+ */
+function requireAuth(next) {
+	return function(req, res) {
+		if (req.isAuthenticated())
+			next(req, res);
+		else
+			res.status(401).send("Unauthorized");
+	};
+}
