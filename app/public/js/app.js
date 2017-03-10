1471bdd59b674e7f19f673e9a12c99cfd5b29877
Will Bennion <will.bennion@outlook.com>
2017-03-10 17:29:27 +0000

    Added modal window to HTML

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -1,12 +1,28 @@
-function createSubscriptionListItem() {
-	var newItem = $($('#subscription-list-item').html());
-	$('#subscription-list').append(newItem);
+$(function() {
+	$('input[data-date-type]').datepicker({
+		autoclose: true,
+		format: "dd/mm/yyyy",
+		todayHighlight: true
+	});
+});
+
+function getSubscriptionFormVal() {
+	var model = {
+		delivery_days: 0
+	};
+
+	model.publication_id = $('[data-subscription-binding="publication_id"]').val();
+	model.start_date = dateConverter_yyyymmdd($('[data-subscription-binding="start_date"]').val());
+	model.end_date = $('#subscription-edit-start-date-null').is(':checked') ? null : dateConverter_yyyymmdd($('[data-subscription-binding="end_date"]').val());
+
+	$('[data-subscription-binding="delivery_days"] input[type="checkbox"]:checked').each(function() {
+		model.delivery_days += +$(this).val();
+	});
+
+	return model;
 }
 
-function createSuspensionListItem() {
-	var newItem = $($('#suspension-list-item').html());
-	$('#suspension-list').append(newItem);
+function dateConverter_yyyymmdd(ddmmyyyy) {
+	var parts = ddmmyyyy.split("/");
+	return parts[2] + "-" + parts[1] + "-" + parts[0];
 }
-
-for (var i = 4; i--;) createSubscriptionListItem();
-for (var i = 3; i--;) createSuspensionListItem();
