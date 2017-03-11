6ff55dd2a25b120085d6e972534fdae93be37bfc
Will Bennion <will.bennion@outlook.com>
2017-03-11 16:50:39 +0000

    Implemented Vue. Subscription add and delete working. Still needs error handling and showing errors to user, editing subscriptions and dynamically loading of publications.

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -1,3 +1,115 @@
+new Vue({
+	el: '#app',
+
+	data: {
+		// Main data
+		user: {},
+		subscriptions: [],
+		suspensions: [],
+
+		// Client only data
+		subscriptionEditId: -1
+	},
+
+	mounted: function() {
+		this.fetchUser();
+		this.fetchSubscriptions();
+		this.fetchSuspensions();
+	},
+
+	methods: {
+		fetchUser: function() {
+			var thisVue = this;
+			$.getJSON('/user').done(function(d) {
+				thisVue.$data.user = d.user;
+			});
+		},
+
+		// --------------------- //
+		// Subscription methods //
+		// ------------------- //
+		fetchSubscriptions: function() {
+			var thisVue = this;
+			$.getJSON('/subscriptions').done(function(d) {
+				thisVue.$data.subscriptions = d.results;
+			});
+		},
+
+		// Add the current data in the modal as a new subscription
+		addSubscription: function() {
+			var thisVue = this;
+			var isUpdate = false;//thisVue.$data.subscriptionEditId > 0; // True if updating existing subscription, false if adding new
+			
+			$.ajax({
+				url: '/subscriptions' + (isUpdate ? "/" + thisVue.$data.subscriptionEditId : ""),
+				method: isUpdate ? "PUT" : "POST",
+				data: $.param(getSubscriptionFormVal())
+
+			}).done(function(d) {
+				if (d.success)
+					return $.getJSON('/subscriptions/' + d.id).done(function(d) {
+						thisVue.$data.subscriptions.push(d.result);
+						$('#edit-subscription-mdl').modal("hide");
+					});
+
+			}).fail(function(err) {
+				console.error(err);
+			});
+		},
+
+		// Handler for delete button to delete the clicked subscription
+		deleteSubscription: function(e) {
+			var thisVue = this;
+			var targetLi = $(e.currentTarget).closest('[data-subscription-id]');
+			var subscriptionId = targetLi.data('subscription-id'), subscriptionIndex = targetLi.index();
+
+			if (confirm("Are you sure?")) {
+				$.ajax({
+					url: '/subscriptions/' + subscriptionId,
+					method: "DELETE"
+
+				}).done(function(d) {
+					if (d.success)
+						thisVue.$data.subscriptions.splice(subscriptionIndex, 1);
+
+				}).fail(function(err) {
+					alert("Failed to delete: " + err);
+				});
+			}
+		},
+
+		openSubscriptionModal: function(id) {
+			this.$data.subscriptionEditId = id;
+			$('#edit-subscription-mdl').modal("show");
+		},
+
+		// ------------------- //
+		// Suspension methods //
+		// ----------------- //
+		fetchSuspensions: function() {
+			var thisVue = this;
+			$.getJSON('/suspensions').done(function(d) {
+				thisVue.$data.suspensions = d.results;
+			});
+		},
+
+		// ----- //
+		// Misc //
+		// --- //
+		// Takes a delivery_days number and returns a HTML string in the form SMTWTFS with strikethrough on letters that are inactive
+		getDayString: function(num) {
+			var html = "";
+			var days = "SMTWTFS";
+			for (var i = 0; i < 7; i++) {
+				var dayVal = Math.pow(2, i);
+				var tagName = (num & dayVal) == dayVal ? "b" : "s";
+				html += (i == 0 ? "" : " " /*Add a space on all but first*/) + "<" + tagName + ">" + days[i] + "</" + tagName + ">";
+			}
+			return html;
+		}
+	}
+});
+
 $(function() {
 	$('input[data-date-type]').datepicker({
 		autoclose: true,
@@ -13,7 +125,8 @@ function getSubscriptionFormVal() {
 
 	model.publication_id = $('[data-subscription-binding="publication_id"]').val();
 	model.start_date = dateConverter_yyyymmdd($('[data-subscription-binding="start_date"]').val());
-	model.end_date = $('#subscription-edit-start-date-null').is(':checked') ? null : dateConverter_yyyymmdd($('[data-subscription-binding="end_date"]').val());
+	if ($('#subscription-edit-start-date-null').is(':checked'))
+		model.end_date = dateConverter_yyyymmdd($('[data-subscription-binding="end_date"]').val());
 
 	$('[data-subscription-binding="delivery_days"] input[type="checkbox"]:checked').each(function() {
 		model.delivery_days += +$(this).val();
@@ -23,6 +136,7 @@ function getSubscriptionFormVal() {
 }
 
 function dateConverter_yyyymmdd(ddmmyyyy) {
+	if (ddmmyyyy == null || ddmmyyyy == "") return "";
 	var parts = ddmmyyyy.split("/");
 	return parts[2] + "-" + parts[1] + "-" + parts[0];
 }
