010af4d3c6fd8c0e1816ed6b8eeb6fec0f495bb6
Will Bennion <will.bennion@outlook.com>
2017-03-14 00:11:40 +0000

    Added the admin page - named "dashboard". dashboard.html is served instead of app.html when the logged in user is an admin.

--- custom_modules/routing.js
+++ custom_modules/routing.js
@@ -3,7 +3,7 @@ const appDir = require('path').resolve("./app");
 module.exports = function(app) {
 	app.get('/', (req, res) => {
 		if (req.isAuthenticated())
-			res.sendFile(appDir + "/html/app.html"); // This will be changed to res.render eventually
+			res.sendFile(appDir + "/html/" + (req.user.is_admin ? "dashboard" : "app") + ".html");
 		else
 			res.sendFile(appDir + "/html/login.html");
 	});
