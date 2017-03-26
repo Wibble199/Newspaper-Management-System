9d936b103add720980f48af026b3332741194795
Will Bennion <will.bennion@outlook.com>
2017-03-26 18:33:51 +0100

    Updated login form with error message

--- app/public/js/login.js
+++ app/public/js/login.js
@@ -1,17 +1,31 @@
 $(document).ready(function() {
 	$('#login-form').on("submit", function(e) {
-		e.preventDefault();
+		e.preventDefault(); // Stop default form action
 
+		$('.login-container').loadingOverlay(true); // Show loading icon
 		$.ajax({
 			method: "POST",
 			dataType: "json",
 			url: "/login",
 			data: $('#login-form').serialize(),
+
 		}).done(function(data) {
 			if (data.success)
-				location.reload();
-			else
-				console.log(data.message);
+				location.reload(); // Reload to make server serve app instead of login
+			else {
+				animateLogin(); // Shake form
+				$('[name="password"]').val(""); // Clear PW box
+				$('.alert') // Update alert message
+					.removeClass('alert-success').addClass('alert-danger')
+					.html('<span class="glyphicon glyphicon-exclamation-sign"></span> Incorrect login details');
+				$('.login-container').loadingOverlay(false); // Hide loading icon
+			}
 		})
 	});
 });
+
+// Shake the login container box
+function animateLogin() {
+	var login = $('.login-container').addClass("animate");
+	setTimeout(function() { login.removeClass("animate"); }, 400); // .animate has animation that runs for 400ms, so after that time remove the class to allow it to be re-animated
+}
