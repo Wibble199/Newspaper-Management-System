0c1fb9bbc4c3df1d29859da9796d27effff4977a
Will Bennion <will.bennion@outlook.com>
2017-03-29 02:17:57 +0100

    Delivery list now uses suspension periods

--- app/sql/generate-delivery-list.sql
+++ app/sql/generate-delivery-list.sql
@@ -1,4 +1,4 @@
-SELECT customer_id, publication_id, c.name AS customer_name, c.address1, c.address2, c.address3, c.address4, c.postcode, pub.name AS publication_name
+SELECT sub.customer_id, publication_id, c.name AS customer_name, c.address1, c.address2, c.address3, c.address4, c.postcode, pub.name AS publication_name
 	FROM subscriptions AS sub
 
 INNER JOIN publications AS pub
@@ -7,8 +7,11 @@ INNER JOIN publications AS pub
 INNER JOIN customers AS c
 	ON sub.customer_id = c.id
 
+LEFT JOIN suspensions AS susp
+	ON susp.start_date <= ::day AND susp.end_date >= ::day AND susp.customer_id = sub.customer_id
+
 WHERE sub.start_date < ::day AND (sub.end_date IS NULL OR sub.end_date > ::day) -- Ensure subscription is active
-	-- TODO: TAKE SUSPENSIONS INTO ACCOUNT
+	AND susp.id IS NULL -- Take suspensions into account
 	AND sub.delivery_days & POW(2, DAYOFWEEK(::day) - 1) = POW(2, DAYOFWEEK(::day) - 1) -- Only fetch subscriptions due on the the same day of week as the day parameter
 
 ORDER BY sub.customer_id, pub.id; -- Sort by the customer so that it is easier for the NodeJS server to parse
